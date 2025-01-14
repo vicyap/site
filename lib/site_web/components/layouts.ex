e9a2945653e11e8d8c2c3059f732f05e3111f309
Victor Yap <vicyap24@gmail.com>
2025-01-14 13:23:01 -0600

    Initial Commit
    mix archive.install hex phx_new
    mix phx.new site

---
+++ lib/site_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule SiteWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use SiteWeb, :controller` and
+  `use SiteWeb, :live_view`.
+  """
+  use SiteWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/site_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule SiteWeb.PageController do
+  use SiteWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
